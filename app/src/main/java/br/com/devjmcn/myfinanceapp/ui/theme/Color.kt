e82a950d6e7d004eca297834886744a8d58c09ce
João Mourato <joaomouratocn@gmail.com>
2024-11-27 10:44:40 -0300

    criada paleta de cores

--- app/src/main/java/br/com/devjmcn/myfinanceapp/ui/theme/Color.kt
+++ app/src/main/java/br/com/devjmcn/myfinanceapp/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package br.com.devjmcn.myfinanceapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BluePrimary = Color(0xFF2196F3)
+val BluePrimaryVariant = Color(0xFF1976D2)
+val BlueSecondary = Color(0xFF03A9F4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundLight = Color(0xFFFFFFFF)
+val SurfaceLight = Color(0xFFF5F5F5)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+
+val TextPrimaryLight = Color(0xFF212121)
+val TextSecondaryLight = Color(0xFF757575)
+val TextPrimaryDark = Color(0xFFFFFFFF)
+val TextSecondaryDark = Color(0xFFBDBDBD)
--- app/src/main/java/br/com/devjmcn/myfinanceapp/ui/theme/Theme.kt
+++ app/src/main/java/br/com/devjmcn/myfinanceapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package br.com.devjmcn.myfinanceapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,33 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorScheme = lightColorScheme(
+    primary = BluePrimary,
+    onPrimary = Color.White,
+    primaryContainer = BluePrimaryVariant,
+    onPrimaryContainer = Color.White,
+    secondary = BlueSecondary,
+    onSecondary = Color.White,
+    background = BackgroundLight,
+    onBackground = TextPrimaryLight,
+    surface = SurfaceLight,
+    onSurface = TextPrimaryLight,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = BluePrimary,
+    onPrimary = Color.Black,
+    primaryContainer = BluePrimaryVariant,
+    onPrimaryContainer = Color.Black,
+    secondary = BlueSecondary,
+    onSecondary = Color.Black,
+    background = BackgroundDark,
+    onBackground = TextPrimaryDark,
+    surface = SurfaceDark,
+    onSurface = TextPrimaryDark,
 )
 
 @Composable
